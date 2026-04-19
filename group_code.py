4eed21954a73fe6cddd50a97728921b596838567
EvanEames <evan.eames@lmu.de>
2026-04-19 23:25:03 +0200

    formatting

--- group_code.py
+++ group_code.py
@@ -1,4 +1,4 @@
-def string_to_numbers(string: str, mapping: dict) -> list[int]:
+def string_to_numbers(string: str, mapping: dict[str:int]) -> list[int]:
     """
     Description
     The function takes a string and a mapping dictionary.
@@ -23,5 +23,5 @@ def string_to_numbers(string: str, mapping: dict) -> list[int]:
     return number_list
 
 
-dict = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15,
+mapping = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15,
         "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
