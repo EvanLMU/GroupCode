e9e5ad0f0b2c636b8e04945cdf92f8d12b95230c
EvanEames <evan.eames@lmu.de>
2026-04-19 14:04:00 +0200

    function finished and print statement

--- group_code.py
+++ group_code.py
@@ -1,4 +1,4 @@
-def string_to_numbers(string: str, mapping: dict) -> list[int]:
+def string_to_numbers(string: str, mapping: dict[str:int]) -> list[int]:
     """
     Description
     The function takes a string and a mapping dictionary.
@@ -17,11 +17,18 @@ def string_to_numbers(string: str, mapping: dict) -> list[int]:
     string_to_numbers(string="what's up?", mapping = {"a":1, "b":2, "c":3, etc...})
     returns: [23, 8, 1, 20, -1, 19, -1, 21, 16, -1]
     """
-
-    # Write code here
+    
+    number_list = []
+    for char in list(string):
+        if char not in mapping_dict.keys():
+            number_list.append(-1)
+        else:
+            number_list.append(mapping_dict[char.lower()])
 
     return number_list
 
 
-dict = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15,
+mapping_dict = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15,
         "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
+
+print(string_to_numbers("Hey, this is just a test case. What's up?", mapping_dict))
