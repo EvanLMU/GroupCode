d863d8932e12ae25533e723aaa2785cc5d5454e4
Evan <evaneames7@gmail.com>
2026-04-19 13:20:57 +0200

    first commit

---
+++ group_code.py
@@ -0,0 +1,27 @@
+def string_to_numbers(string: str, mapping: dict) -> list[int]:
+    """
+    Description
+    The function takes a string and a mapping dictionary.
+    The code returns a list in which the characters of the string have been changed to integers based on the mapping
+    dictionary.
+    Any character that is not found in dictionary keys is assigned the values -1.
+
+    Input
+    string: any str
+    mapping: dict type in which keys are characters and the keys are integer values.
+
+    Output
+    list[int]: the string converted to numbers
+
+    Example
+    string_to_numbers(string="what's up?", mapping = {"a":1, "b":2, "c":3, etc...})
+    returns: [23, 8, 1, 20, -1, 19, -1, 21, 16, -1]
+    """
+
+    # Write code here
+
+    return number_list
+
+
+dict = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15,
+        "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
