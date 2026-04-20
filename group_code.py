7beb66ddbd752eb9f3e25da8be4f638c8be965d2
ecembuyukhan <ecembuyukhan1@gmail.com>
2026-04-20 12:46:42 +0200

    done

--- group_code.py
+++ group_code.py
@@ -19,9 +19,24 @@ def string_to_numbers(string: str, mapping: dict[str:int]) -> list[int]:
     """
 
     # Write code here
+    number_list = []
+
+    for i in string:
+        if i in mapping:
+
+            n = mapping[i]
+
+        else:
+
+            n = -1
+
+        number_list.append(n)
 
     return number_list
 
 
 mapping = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15,
         "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
+
+
+print(string_to_numbers(string="what's up?", mapping=mapping))
