875afe6c0ad7feb1fde45f46191da64e5cb78d6d
y0oseph <joseph.richter@web.de>
2026-04-20 12:42:52 +0200

    try

--- group_code.py
+++ group_code.py
@@ -18,10 +18,30 @@ def string_to_numbers(string: str, mapping: dict[str:int]) -> list[int]:
     returns: [23, 8, 1, 20, -1, 19, -1, 21, 16, -1]
     """
 
+
     # Write code here
+    number_list = []
+    
+    string = string.split()
+    print(string)
+
+    for n in string:
+        for i in n:
+            print(i)
+
+            
+            if i in mapping.keys():
+                
+                number_list.append(mapping[i])
+
+            else:
+                number_list.append(-1)
 
     return number_list
 
 
 mapping = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15,
         "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
+
+print(string_to_numbers(string="what's up?", mapping = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15,
+        "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}))
