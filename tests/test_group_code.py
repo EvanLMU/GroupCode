85e1a80700d14e4fe1fd0bc33cb66412e7471dff
EvanEames <evan.eames@lmu.de>
2026-04-27 00:13:04 +0200

    simplified unit tests

--- tests/test_group_code.py
+++ tests/test_group_code.py
@@ -1,17 +1,15 @@
-import unittest
-
 from group_code import string_to_numbers
 
 
-class GroupCodeTestCase(unittest.TestCase):
-    def test_string_to_numbers_converts_known_chars(self):
-        mapping = {"a": 1, "b": 2, "c": 3}
-        self.assertEqual(string_to_numbers("abc", mapping), [1, 2, 3])
+#class GroupCodeTestCase(unittest.TestCase):
+def test_string_to_numbers_converts_known_chars():
+    mapping = {"a": 1, "b": 2, "c": 3}
+    assert(string_to_numbers("abc", mapping) == [1, 2, 3])
 
-    def test_string_to_numbers_maps_unknown_chars_to_minus_one(self):
-        mapping = {"a": 1, "b": 2}
-        self.assertEqual(string_to_numbers("ab?", mapping), [1, 2, -1])
+def test_string_to_numbers_maps_unknown_chars_to_minus_one():
+    mapping = {"a": 1, "b": 2}
+    assert(string_to_numbers("ab?", mapping) == [1, 2, -1])
 
 
-if __name__ == "__main__":
-    unittest.main()
+#if __name__ == "__main__":
+#    unittest.main()
