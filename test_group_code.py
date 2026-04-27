23cc75e705b096ac8c4384226931bd8e945057f2
EvanEames <evan.eames@lmu.de>
2026-04-27 10:18:44 +0200

    update gitignore

--- test_group_code.py
+++ test_group_code.py
@@ -1,7 +1,5 @@
 from group_code import string_to_numbers
 
-
-#class GroupCodeTestCase(unittest.TestCase):
 def test_string_to_numbers_converts_known_chars():
     mapping = {"a": 1, "b": 2, "c": 3}
     assert(string_to_numbers("abc", mapping) == [1, 2, 3])
@@ -9,7 +7,3 @@ def test_string_to_numbers_converts_known_chars():
 def test_string_to_numbers_maps_unknown_chars_to_minus_one():
     mapping = {"a": 1, "b": 2}
     assert(string_to_numbers("ab?", mapping) == [1, 2, -1])
-
-
-#if __name__ == "__main__":
-#    unittest.main()
